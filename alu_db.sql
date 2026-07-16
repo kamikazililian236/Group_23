21e26eac97527518918423df623020aafcff7a4c
mwnaikifelix-01 <f.mwaniki@alustudent.com>
2026-07-16 17:17:53 +0200

    insertion of details

--- alu_db.sql
+++ alu_db.sql
@@ -6,3 +6,12 @@ CREATE TABLE Students(
     enrollment_date date
 );
 
+select * from Students;
+insert from Students(student_id, name, email, classroom_id, enrollment_date)
+VALUES
+(123, 'Lilian Kamikazi', 'l.kamikazi@alustudent.com', 001, '2026-01-05'),
+(234, 'Kingsley Kipkoech', 'k.kipkoech@alustudent', 002, '2026-01-06'),
+(542, 'Vanessa Kampiire', 'v.kampiire@alustudent.com' 001, '2026-01-06'),
+(656, 'Gabriel Agabo', 'g.agabo@alustudent.com', 002, '2026-01-06'),
+(768, 'Rita Akariza', 'r.akariza@alustudent.com', 003, '2026-01-05'),
+(876, 'Felix Mwaniki', 'f.mwaniki@alustudent', 004, '2026-01-05')
