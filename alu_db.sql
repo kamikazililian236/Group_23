e84ba24c901b19953594b51315580dbe15930437
rakariza <r.akariza@alustudent.com>
2026-07-16 20:52:05 +0200

    Modify Courses table and update course data
    Updated course entries and modified constraints in the Courses table.

--- alu_db.sql
+++ alu_db.sql
@@ -1,6 +1,3 @@
--- =========================================
--- MEMBER 4: COURSES TABLE
--- =========================================
 
 CREATE TABLE Courses (
     course_id INT PRIMARY KEY,
@@ -9,23 +6,26 @@ CREATE TABLE Courses (
     faculty_id INT,
     classroom_id INT,
     FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id),
-    FOREIGN KEY (classroom_id) REFERENCES Classroom(classroom_id)
+    FOREIGN KEY (classroom_id) REFERENCES classroom(classroom_id)
 );
 
 INSERT INTO Courses (course_id, course_name, credits, faculty_id, classroom_id)
 VALUES
-(1, 'Introduction to Programming', 3, 1, 1),
-(2, 'Database Systems', 4, 2, 2),
-(3, 'Data Structures', 3, 1, 3),
-(4, 'Business Analytics', 3, 3, 2),
-(5, 'Web Development', 4, 2, 1);
+(1, 'Linux Introduction', 3, 4, 1),
+(2, 'Python Programming', 4, 1, 2),
+(3, 'Web Infrastructure', 4, 5, 3),
+(4, 'Frontend Web Development', 4, 6, 2),
+(5, 'Self Leadership and Dynamics', 3, 3, 5),
+(6, 'Temporary Course', 2, 2, 1);
 
 UPDATE Courses
-SET credits = 4
+SET credits = 5
 WHERE course_id = 3;
 
 DELETE FROM Courses
-WHERE course_id = 5;
+WHERE course_id = 6;
 
+SELECT * FROM Courses
+WHERE credits = 4;
 SELECT * FROM Courses
 WHERE credits >= 4;
