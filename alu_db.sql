a8778adcb08634aba668c434afc388c8378f6cf1
Gabriel Agaba <g.agaba@alustudent.com>
2026-07-16 20:09:06 +0200

    Facaluty

--- alu_db.sql
+++ alu_db.sql
@@ -1,1 +1,8 @@
 CREATE  DATABASE Group_23;
+CREATE TABLE Faculty (
+    faculty_id INT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    department VARCHAR(100),
+    email VARCHAR(100)
+);
