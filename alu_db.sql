0b3495a1192827f8e6060a9a30e0b95cb3573b2a
Lilian Kamikazi <l.kamikazi@alustudent.com>
2026-07-16 18:24:35 -0700

    Creating the classroom table

--- alu_db.sql
+++ alu_db.sql
@@ -1,1 +1,16 @@
 CREATE  DATABASE Group_23;
+create table classroom(
+    classroom_id int primary key,
+    room_number varchar(10),
+    building varchar(50),
+    capacity int
+);
+
+insert into classroom(classroom_id,room_number,building,capacity)
+values
+(001,03,'South East',50),
+(002,04,'Ilinios Chamber',45),
+(003,05,'Social Commons',25),
+(004,06,'Berlin corner',76),
+(005,07,'Frederick rooms',37),
+(006,08,'Leadership center',22)
